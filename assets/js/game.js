07e2c1b61d2203d1fb0869a69c1dde27aa228eb1
Joshua Rangai <rangaij0116@hotmail.com>
2022-03-04 14:44:47 -0600

    console logs

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,11 @@
-window.alert("This is an alert! JavaScript is running!");
+// player name
+var playerName = window.prompt("What is your robot's name?");
+console.log(playerName);
+
+// this creates a fight function
+
+function fight() {
+    window.alert("The fight has begun!");
+}
+
+// fight();
